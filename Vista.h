000a0ed364165746f457b7b38b6dde558dc6ffba
Schurches <steveniglesias@hotmail.com>
2016-03-20 21:44:58 -0500

    Agregado diseño e imagenes
    -Cambiado labels por imagenes
    -Agregado logo del juego
    -Agregado fondo y fotos extras
    -Agregado groupbox para organizar
    -Modificado el color del groupbox y panel

--- Vista.h
+++ Vista.h
@@ -35,14 +35,21 @@ namespace PanelsitoLogin {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^  label1;
+
 	protected:
-	private: System::Windows::Forms::Button^  button1;
+
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::TextBox^  textBox2;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Button^  button2;
+
+
+
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::PictureBox^  pictureBox3;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::PictureBox^  pictureBox4;
+	private: System::Windows::Forms::PictureBox^  pictureBox5;
+	private: System::Windows::Forms::PictureBox^  pictureBox6;
 
 	private:
 		/// <summary>
@@ -57,100 +64,150 @@ namespace PanelsitoLogin {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Vista::typeid));
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->pictureBox5 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox6 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox5))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox6))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(90, 37);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Registro";
-			this->label1->Click += gcnew System::EventHandler(this, &Vista::label1_Click);
-			// 
-			// button1
-			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(12, 202);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(231, 42);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Verificar";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Vista::button1_Click);
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(114, 100);
+			this->textBox1->BackColor = System::Drawing::Color::DarkOrange;
+			this->textBox1->Location = System::Drawing::Point(19, 97);
+			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(132, 20);
+			this->textBox1->Size = System::Drawing::Size(270, 32);
 			this->textBox1->TabIndex = 2;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(114, 149);
+			this->textBox2->BackColor = System::Drawing::Color::Orange;
+			this->textBox2->Location = System::Drawing::Point(19, 205);
+			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(132, 20);
+			this->textBox2->PasswordChar = '*';
+			this->textBox2->Size = System::Drawing::Size(270, 32);
 			this->textBox2->TabIndex = 3;
 			// 
-			// label2
+			// pictureBox1
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(12, 103);
-			this->label2->Name = L"label2";
-			this->label2->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
-			this->label2->Size = System::Drawing::Size(96, 13);
-			this->label2->TabIndex = 4;
-			this->label2->Text = L"Nombre de usuario";
-			this->label2->Click += gcnew System::EventHandler(this, &Vista::label2_Click);
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(15, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(261, 85);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 7;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &Vista::pictureBox1_Click);
 			// 
-			// label3
+			// pictureBox2
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(15, 156);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(61, 13);
-			this->label3->TabIndex = 5;
-			this->label3->Text = L"Contraseña";
-			this->label3->Click += gcnew System::EventHandler(this, &Vista::label3_Click);
+			this->pictureBox2->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(6, 35);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(302, 50);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 8;
+			this->pictureBox2->TabStop = false;
 			// 
-			// button2
+			// pictureBox3
 			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(12, 272);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(109, 33);
-			this->button2->TabIndex = 6;
-			this->button2->Text = L"Volver al Login";
-			this->button2->UseVisualStyleBackColor = true;
+			this->pictureBox3->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(19, 146);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(270, 53);
+			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox3->TabIndex = 9;
+			this->pictureBox3->TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(200)), static_cast<System::Int32>(static_cast<System::Byte>(170)),
+				static_cast<System::Int32>(static_cast<System::Byte>(120)), static_cast<System::Int32>(static_cast<System::Byte>(100)));
+			this->groupBox1->Controls->Add(this->pictureBox5);
+			this->groupBox1->Controls->Add(this->pictureBox3);
+			this->groupBox1->Controls->Add(this->pictureBox2);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Location = System::Drawing::Point(320, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(317, 369);
+			this->groupBox1->TabIndex = 10;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Crea una nueva cuenta";
+			// 
+			// pictureBox5
+			// 
+			this->pictureBox5->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox5->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox5.Image")));
+			this->pictureBox5->Location = System::Drawing::Point(6, 277);
+			this->pictureBox5->Name = L"pictureBox5";
+			this->pictureBox5->Size = System::Drawing::Size(305, 82);
+			this->pictureBox5->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox5->TabIndex = 10;
+			this->pictureBox5->TabStop = false;
+			this->pictureBox5->Click += gcnew System::EventHandler(this, &Vista::pictureBox5_Click);
+			// 
+			// pictureBox4
+			// 
+			this->pictureBox4->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox4.Image")));
+			this->pictureBox4->Location = System::Drawing::Point(12, 109);
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->Size = System::Drawing::Size(230, 197);
+			this->pictureBox4->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox4->TabIndex = 11;
+			this->pictureBox4->TabStop = false;
+			// 
+			// pictureBox6
+			// 
+			this->pictureBox6->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox6->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox6.Image")));
+			this->pictureBox6->Location = System::Drawing::Point(26, 338);
+			this->pictureBox6->Name = L"pictureBox6";
+			this->pictureBox6->Size = System::Drawing::Size(96, 63);
+			this->pictureBox6->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox6->TabIndex = 12;
+			this->pictureBox6->TabStop = false;
 			// 
 			// Vista
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(271, 317);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label1);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(668, 404);
+			this->Controls->Add(this->pictureBox6);
+			this->Controls->Add(this->pictureBox4);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->pictureBox1);
 			this->Name = L"Vista";
-			this->Text = L"Vista";
+			this->Text = L"Nueva cuenta";
 			this->Load += gcnew System::EventHandler(this, &Vista::Vista_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox5))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox6))->EndInit();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 
@@ -280,25 +337,22 @@ namespace PanelsitoLogin {
 		}
 #pragma endregion
 	private: System::Void Vista_Load(System::Object^  sender, System::EventArgs^  e) {
-				 String^T = "Astrobix,awaker2130";
-				 int M = HallarComas(T);
-				 MessageBox::Show(T->Substring(0,M));
-				 MessageBox::Show(T->Substring(M+1,(T->Length-(M+1))));
-		//		 StreamWriter^ File = gcnew StreamWriter("Lista De Usuarios", true);
-		//		 File->WriteLine("Astrobix,awaker2130");
-				 
+				// groupBox1->BackColor = color
 	}
 
 
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-			 verificarLogin();
-
+			 
 }
-private: System::Void label3_Click(System::Object^  sender, System::EventArgs^  e) {
-}
-private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
-}
+		 private: System::Void pictureBox5_Click(System::Object^  sender, System::EventArgs^  e) {
+					  verificarLogin();
+		 }
 private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 }
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+
 };
 }
